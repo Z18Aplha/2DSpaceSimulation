7ff33dd4b6f6f364ca61abb151df36ff20e51070
RobertWalstab <robert.walstab@gmail.com>
2019-04-21 23:36:53 +0200

    Added Collision Control an changed Obstacles to Polygons

--- Obstacles2D.py
+++ Obstacles2D.py
@@ -1,9 +1,8 @@
 class Obstacles2D:
-    def __init__(self, spawn_x, spawn_y, size_x, size_y, color):
+    def __init__(self, spawn_x, spawn_y, corners: list, color):
         # PROPERTIES
         self.color = color
         self.spawn = [spawn_x, spawn_y]
         self.position = []  # in m - later: instantiation of 2d space with dates in metres
-        self.length = size_x  # [length] = m
-        self.width = size_y  # [width] = m
+        self.edges = corners
 
